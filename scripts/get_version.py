e8d304b60376e5ec32cf297efb915bd0ff7c3167
Peder Toftegaard Olsen <peder@morgendagen.dk>
2024-01-18 18:44:59 +0100

    Do not rename firmware file.

--- scripts/get_version.py
+++ scripts/get_version.py
@@ -19,6 +19,3 @@ print(f'Using version {firmware_version} for the build')
 env.Append(CPPDEFINES=[
     f'BUILD_VERSION={firmware_version}',
 ])
-
-# Set the output filename to the name of the board and the version
-env.Replace(PROGNAME=f'firmware_{env["PIOENV"]}_{firmware_version.replace(".", "_")}')
